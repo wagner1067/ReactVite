ae37afd142cd04e15213d262cd675fc33a074c3d
wagner1067 <barra-wag@hotmail.com>
2024-09-03 17:11:21 -0300

    create ft Footer and Button

--- ReactAulaNova/arq-wagner/src/pages/Home.jsx
+++ ReactAulaNova/arq-wagner/src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import Button from "../components/Button/Button";
 
 function Home() {
     return (
@@ -6,6 +8,8 @@ function Home() {
             <Header />
             <h1>Home</h1>
             <p>Subtitle home</p>
+            <Button buttonStyle="secondary" arrow> Teste</Button>
+            <Footer />
         </>
     )
 }
