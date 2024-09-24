b8ebd81d328494ca1f2efea659d8d930485684d7
wagner1067 <barra-wag@hotmail.com>
2024-09-23 22:36:24 -0300

    add useEfect e useStates

--- ReactAulaNova/arq-wagner/src/App.jsx
+++ ReactAulaNova/arq-wagner/src/App.jsx
@@ -1,13 +1,18 @@
-import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
+
 //PAGES
 import Home from '../src/pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 
+//UTILS
+import ScrollToTop from './utils/ScrollTop';
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
--- ReactAulaNova/arq-wagner/src/components/Header/Header.jsx
+++ ReactAulaNova/arq-wagner/src/components/Header/Header.jsx
@@ -1,16 +1,32 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Header.css';
 
 //ASSETS
+import './Header.css';
 import Logo from '../../assets/logo-dnc.svg';
 
+//COMPONENTS
+import Button from '../Button/Button'
+
 function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+    const tottleMenu = () => {
+        setIsOpen(!isOpen)
+    }
     return (
         <header>
             <div className="container">
                 <div className="al-center d-flex jc-space-between">
                     <Link to="/"><img src={Logo} /></Link>
-                    <nav>
+                    <div className='mobile-menu'>
+                        <Button buttonStyle="secondary" onClick={tottleMenu}>
+                            Menu
+                        </Button>
+                    </div>
+                    <nav className={`${isOpen ? 'open' : ''}`}>
+                        <Button buttonStyle="unstyled" className="mobile-menu close-btn" onClick={tottleMenu}>
+                            X
+                        </Button>
                         <ul className="d-flex">
                             <li><Link to="/">Home</Link></li>
                             <li><Link to="/about">About</Link></li>
