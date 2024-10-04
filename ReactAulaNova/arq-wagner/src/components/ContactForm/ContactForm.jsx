fbeb41a49f81cedf9398a8d170a4c107dbda526a
wagner1067 <barra-wag@hotmail.com>
2024-10-03 23:06:15 -0300

    add url api web3forms

--- ReactAulaNova/arq-wagner/src/components/ContactForm/ContactForm.jsx
+++ ReactAulaNova/arq-wagner/src/components/ContactForm/ContactForm.jsx
@@ -11,13 +11,34 @@ function ContactForm() {
         message: ''
     })
     const [isFormValid, setIsFormValid] = useState(false)
+    const [formSubmitLoading, setFormSubmitLoading] = useState(false)
+    const [forSubmitted, setForSubmitted] = useState(false)
+
 
     const handleSubmit = async (e) => {
         e.preventDefault()
         if (isFormValid) {
-            null
-        }
+            setFormSubmitLoading(true)
+            try {
+                const response = await fetch('https://api.web3forms.com/submit', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ ...formData, access_key: "473c1a76-001f-4df0-b0c6-6cdf2b973865" })
+                })
 
+                if (response.ok) {
+                    setForSubmitted(true)
+                } else {
+                    alert('Erro ao Enviar!')
+                }
+            } catch (e) {
+                alert('Erro:', e)
+            } finally {
+                setFormSubmitLoading(false)
+            }
+        }
 
     }
     useEffect(() => {
@@ -45,7 +66,7 @@ function ContactForm() {
         <div className="container">
             <div className="contact-form d-flex fd-column al-center">
                 <h2>We love meeting new people and helping them.</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="d-flex form-group">
                         <input
                             className="form-input"
@@ -75,7 +96,8 @@ function ContactForm() {
                         ></textarea>
                     </div>
                     <div className="al-center d-flex jc-end form-group">
-                        <Button type="submit" buttonStyle="secondary" disabled={!isFormValid} >
+                        {forSubmitted && <p className='text-primary'>Sucesso</p>}
+                        <Button type="submit" buttonStyle="secondary" disabled={!isFormValid || formSubmitLoading} >
                             Enviar
                         </Button>
                     </div>
